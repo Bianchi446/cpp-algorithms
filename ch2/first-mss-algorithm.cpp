e7b065da9231dfd0e6c90f3756b78148184fd1b6
Bianchi446 <josebianchi221js@gmail.com>
2023-10-05 19:46:57 -0300

    correction

--- ch2/first-mss-algorithm.cpp
+++ ch2/first-mss-algorithm.cpp
@@ -26,11 +26,46 @@ int maxSubSum1(const vector<int> & a)
 }
 
 
-int main() 
+
+
+int maxSubSum2( const vector<int> & a )
+{
+	int maxSum = 0;
+
+	for( std::size_t i = 0; i < a.size( ); ++i )
+	{
+		int thisSum = 0;
+		for( std::size_t j = i; j < a.size( ); ++j )
+		{
+			thisSum += a[ j ];
+
+			if( thisSum > maxSum )
+				maxSum = thisSum;
+		}
+	}
+
+	return maxSum;
+}
+
+
+
+int maxSum(const vector<int> & a)
+{
+	
+}
+
+
+int main()
 {
 	vector<int> a {4, -3, 5, -2, -1, 2, 6, -2};
 	int maxSum;
 
 	maxSum = maxSubSum1( a );
 	cout << "Max sum is " << maxSum << endl; 
+	maxSum = maxSubSum2( a );
+	cout << "Max sum is " << maxSum << endl;
+
+
 }
+
+
